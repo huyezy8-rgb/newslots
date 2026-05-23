4895b521b8011445fb668618debf7aca9594230f
yyx <1103895340@qq.com>
2026-05-24 03:17:37 +0800

    将pdd邀请奖励领取后，补充金额，为第二次转盘准备

--- app/common/service/PddService.php
+++ app/common/service/PddService.php
@@ -408,7 +408,7 @@ class PddService
             $pdd_init_min = Db::name('config')->where('id',66)->value('value');
             $pdd_init_max = Db::name('config')->where('id',67)->value('value');
             $newInvite_reward = rand($pdd_init_min,$pdd_init_max);
-            Db::name('account')->where('user_id', $userId)->update(['pdd_reward' => $newInvite_reward]);
+            Db::name('account')->where('id', $userId)->update(['pdd_reward' => $newInvite_reward]);
             if (!$existingProgress) {
                 // 只有在没有 status=0 的进度记录时才创建新的
                 $nextId = Db::name('pdd_progress')->insertGetId([
