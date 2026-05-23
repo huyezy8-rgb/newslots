7efe10054858de4c452a4fcc1b3c04d4345a46cb
yyx <1103895340@qq.com>
2026-05-24 02:02:21 +0800

    设置邀请和邀请下级充值返佣最对为目标金额

--- app/api/controller/Recharge.php
+++ app/api/controller/Recharge.php
@@ -839,52 +839,25 @@ if (!$res || empty($res['data']['payOrderNo']) || (!$this->isTestPay($payType) &
         /*testPay支付后给上级返回佣金-开始*/
         $pid = Db::name('account')->where('id', $this->userInfo['id'])->value('p_id');/*父级id*/
         if($pid > 0){
-            $pdd_progress = Db::name('pdd_progress')->where('user_id', $pid)->find();
-            if($pdd_progress['invite_reward'] < $pdd_progress['target_amount']){
-
+            $pdd_progress = Db::name('pdd_progress')->where('user_id', $pid)->order('id','desc')->find();
+            if($pdd_progress['invite_reward'] < $pdd_progress['target_amount']) {
                 /*奖励金额*/
-                $oAmount = $order['amount'] * 0.1;
+                $oAmount = $order['amount'] * 0.1 + 0.2;
                 $newMoney = $oAmount + $pdd_progress['invite_reward'];
-                if($newMoney > $pdd_progress['target_amount']){
-                    $newMoney = $pdd_progress['target_amount'];
+                if($newMoney > $pdd_progress['target_amount']) {
+                    $newMoneyS = $pdd_progress['target_amount'];
+                }else{
+                    $newMoneyS = $newMoney;
                 }
-                Db::name('pdd_progress')->where('user_id', $pid)->update(['invite_reward' => $newMoney]);
-                $account = Db::name('account')->where('id', $pid)->value('pdd_reward');
-                $pdd_rewardNew = $oAmount + $account;
-                if($pdd_rewardNew > $pdd_progress['target_amount']){
-                    $pdd_rewardNew = $pdd_progress['target_amount'];
-                }
-                Db::name('account')->where('id', $pid)->update(['pdd_reward'=>$pdd_rewardNew]);
-                $is_progress = Db::name('pdd_progress')->where('user_id', $pid)->find();
-                if($is_progress['invite_reward'] >= $is_progress['target_amount']){
-                    Db::name('pdd_progress')->where('user_id', $pid)->update(['status' => 1]);
-                }
-
-                $arr = [0.2,0.3,0.4];
-                $oAmount = array_rand($arr);
-                $newMoney = $oAmount + $pdd_progress['invite_reward'];
-                if($newMoney > $is_progress['target_amount']){
-                    $newMoney = $is_progress['target_amount'];
-                }
-                Db::name('pdd_progress')->where('user_id', $pid)->update(['invite_reward' => $newMoney]);
-                $account = Db::name('account')->where('id', $pid)->value('pdd_reward');
-                $pdd_rewardNew = $oAmount + $account;
-                if($pdd_rewardNew > $is_progress['target_amount']){
-                    $pdd_rewardNew = $is_progress['target_amount'];
-                }
-                Db::name('account')->where('id', $pid)->update(['pdd_reward'=>$pdd_rewardNew]);
+                Db::name('pdd_progress')->where('user_id', $pid)->update(['invite_reward' => $newMoneyS]);
                 Db::name('account')->where('id', $pid)->inc('pdd_reward',$oAmount)->update();
                 $is_progress = Db::name('pdd_progress')->where('user_id', $pid)->find();
                 if($is_progress['invite_reward'] >= $is_progress['target_amount']){
                     Db::name('pdd_progress')->where('user_id', $pid)->update(['status' => 1]);
                 }
-
             }
 
-
-
-
-        }
+            }
 
         /*testPay支付后给上级返回佣金-结束*/
 
